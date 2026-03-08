5bc79c22db6a16fd66c8378ea95cfc6f1196cf92
BlenMiner <blenminer123@gmail.com>
2026-03-08 14:39:39 +0100

    add close sounds to CloseParentView

--- Assets/Tool/Runtime/UI/CloseParentView.cs
+++ Assets/Tool/Runtime/UI/CloseParentView.cs
@@ -5,10 +5,16 @@ namespace PurrLobby
 {
     public class CloseParentView : MonoBehaviour
     {
+        [SerializeField] private AudioClip[] _closeSounds;
+
         public void Close()
         {
             var view = GetComponentInParent<MonoView>();
-            if (view && view.parentStack) view.CloseMe();
+            if (view && view.parentStack)
+            {
+                Sounds2D.Play(new AudioSession(_closeSounds).WithPitch(1, 0.1f));
+                view.CloseMe();
+            }
         }
     }
 }
